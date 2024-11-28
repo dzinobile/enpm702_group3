6a729f7d2ab94a6db07aba0e2ae144b53f40bc2a
vboxuser <vboxuser@thinkpad.myguest.virtualbox.org>
2024-11-28 12:47:04 -0500

    commit

--- rwa5_3/rwa5_ws/src/group3_rwa5/src/main.cpp
+++ rwa5_3/rwa5_ws/src/group3_rwa5/src/main.cpp
@@ -1,1 +1,18 @@
 #include <rclcpp/rclcpp.hpp>
+#include <bot_waypoint_msgs/msg/bot_waypoint.hpp>
+#include "waypoint_publisher.hpp"
+#include  "waypoint_reacher.hpp"
+
+int main(int argc, char **argv){
+    rclcpp::init(argc, argv);
+    auto node1 = std::make_shared<WaypointPublisher>("node1");
+    auto node2 = std::make_shared<WaypointReacher>("node2");
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+
+    executor.add_node(node1);
+    executor.add_node(node2);
+
+    executor.spin();
+
+}
--- rwa5_3/rwa5_ws/src/group3_rwa5/src/waypoint_reacher.cpp
+++ rwa5_3/rwa5_ws/src/group3_rwa5/src/waypoint_reacher.cpp
@@ -1,1 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
+#include <bot_waypoint_msgs/msg/bot_waypoint.hpp>
+#include "waypoint_reacher.hpp"
+
+void WaypointReacher::receive_int(const bot_waypoint_msgs::msg::BotWaypoint msg) {
+    RCLCPP_INFO_STREAM(this->get_logger(), "Received")
+}
